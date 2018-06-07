ed9ab9f1839e77ab28d09136e00eab49868d6ed1
Lorango-PC\Lorango <tomislav103@gmail.com>
2018-06-07 23:00:05 +0200

    Dodano učitavanje .lab file-a-

--- tvor.m
+++ tvor.m
@@ -3,8 +3,24 @@ Ova skripta æe postepeno biti nadograðivana.
 Od uèitavanja samo jednog fajla do uèitavanja cjelog foldera
 ddsa
 %}
+clc
+clear all
+close all
 
 %%
 
 [samples, sampling_frequency] = audioread('sm04_wav/sm04010103201.wav');
 %%
+window_width = sampling_frequency * 20e-3
+%%
+starting_sample = snimka_start * 0.1e-6 * sampling_frequency
+ending_sample = snimka_stop * 0.1e-6 * sampling_frequency
+%%
+
+file_name = 'sm04_lab/sm04010103201.lab';
+% Load .lab file as table. 
+label = readtable(file_name, 'FileType', 'text',....
+    'Format', '%u%u%s',....
+    'ReadVariableNames', false);
+
+label.Properties.VariableNames = {'Start', 'Stop', 'Label'};
