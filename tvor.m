921fd0c3f69e2b29d8f538455eff206ce2daa116
Lorango-PC\Lorango <tomislav103@gmail.com>
2018-06-07 23:22:12 +0200

    Pronalaženje svih fonema "a" u datoteci.

--- tvor.m
+++ tvor.m
@@ -23,4 +23,14 @@ label = readtable(file_name, 'FileType', 'text',....
     'Format', '%u%u%s',....
     'ReadVariableNames', false);
 
+% Rename proprerty/columns names in table.
 label.Properties.VariableNames = {'Start', 'Stop', 'Label'};
+
+for index_zvuk = 1:size(label, 1)
+    % Comparing two strings. strcmp(s1, s2)
+    % Find all "a" fonems in label file.
+    if strcmp(label.Label(index_zvuk), 'a')
+        disp(label.Start(index_zvuk))
+        
+    end
+end
