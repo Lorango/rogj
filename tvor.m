2fd73460f5aab4be93e804a25827fa0a876dd473
Lorango-PC\Lorango <tomislav103@gmail.com>
2018-06-08 02:30:03 +0200

    Dodani araji koji će sadržavat lpc za svaki pojedini samoglasnik i pohranjivanje u njih. Dodan ispravan način na koji se određuje pomak prozora. Dodan brojač preskočenih fonema. (Kraćih u trajanju od jednog prozora).

--- tvor.m
+++ tvor.m
@@ -8,15 +8,21 @@ clear all
 close all
 
 %%
-M = containers.Map(keySet,valueSet)
+slovo_a = [];
+slovo_e = [];
+slovo_i = [];
+slovo_o = [];
+slovo_u = [];
+
+skip = 0;
 %%
 
 [samples, sampling_frequency] = audioread('sm04_wav/sm04010103201.wav');
 %%
 % Faktor kojim se množe vremena zvukova kako bi se dobil broj uzorka.
-time_factor = sampling_frequency * 0.1e-6
-window_width = sampling_frequency * 20e-3
-window_half_width = window_width  / 2
+time_factor = sampling_frequency * 0.1e-6;
+window_width = sampling_frequency * 20e-3;
+window_half_width = window_width  / 2;
 %%
 
 %%
@@ -44,26 +50,46 @@ for index_zvuk = 1:size(label, 1)
         sample_stop = label.Stop(index_zvuk)* time_factor;
         sample_delta = sample_stop - sample_start;
         
-%         disp(label.Label(index_zvuk));
-%         disp(fonem);
-        
         if sample_delta >= window_width
-            djelj = idivide(sample_delta - window_width, window_half_width)
-            ostatak = rem(sample_delta - window_width, window_half_width)
-            for index_sample = 1:1+djelj
-                [lpc_koef, greska] = lpc(samples(sample_start:sample_stop), 96);
-                slovo_a = [slovo_a; lpc_koef];
+            djelj = idivide(sample_delta, window_half_width);
+            ostatak = rem(sample_delta, window_half_width);
+            broj_koraka = djelj - 2;
+            for index_sample = 0:broj_koraka
+                start = sample_start + index_sample*window_half_width;
+                stop = sample_stop + index_sample*window_half_width;
+                [lpc_koef, greska] = lpc(samples(start:stop), 96);
+                if strcmp(fonem, 'a')
+                    slovo_a = [slovo_a; lpc_koef];
+                elseif strcmp(fonem, 'e')
+                    slovo_e = [slovo_e; lpc_koef];
+                elseif strcmp(fonem, 'i')
+                    slovo_i = [slovo_i; lpc_koef];
+                elseif strcmp(fonem, 'o')
+                    slovo_o = [slovo_o; lpc_koef];
+                elseif strcmp(fonem, 'u')
+                    slovo_u = [slovo_u; lpc_koef];
+                end
             end
+            
             if ostatak > 0
-                [lpc_koef, greska] = lpc(samples(sample_start + ostatak:sample_stop + ostatak), 96);
+                [lpc_koef, greska] = lpc(samples(start + ostatak:stop + ostatak), 96);
+                if strcmp(fonem, 'a')
+                    slovo_a = [slovo_a; lpc_koef];
+                elseif strcmp(fonem, 'e')
+                    slovo_e = [slovo_e; lpc_koef];
+                elseif strcmp(fonem, 'i')
+                    slovo_i = [slovo_i; lpc_koef];
+                elseif strcmp(fonem, 'o')
+                    slovo_o = [slovo_o; lpc_koef];
+                elseif strcmp(fonem, 'u')
+                    slovo_u = [slovo_u; lpc_koef];
+                end
             end
         else
             disp('skip')
-        end
-          % 
-        
-        if strcmp(fonem, 'a')
-            
+            skip = skip + 1;
         end
     end
 end
+disp('Broj preskoèenih samoglasnika jer su prekratki')
+disp(skip)
