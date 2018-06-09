b4a79c7c47db78ecb81e2869ae0f186c09ab99a9
Lorango-PC\Lorango <tomislav103@gmail.com>
2018-06-09 17:54:52 +0200

    Iscrtavanje usporedbe a i a:.

--- zec.m
+++ zec.m
@@ -5,13 +5,19 @@ close all
 %%
 load('lpc\lpc_pro.mat')
 
-%% za a
-%sadrži sve prva tri formanta za sve uzorke
+%% sadrži sve prva tri formanta za sve uzorke
 f_a = zeros(size(slovo_a, 1), 3);
 f_e = zeros(size(slovo_e, 1), 3);
 f_i = zeros(size(slovo_i, 1), 3);
 f_o = zeros(size(slovo_o, 1), 3);
 f_u = zeros(size(slovo_u, 1), 3);
+
+% a: varijacija
+f_aa = zeros(size(slovo_aa, 1), 3);
+f_ee = zeros(size(slovo_ee, 1), 3);
+f_ii = zeros(size(slovo_ii, 1), 3);
+f_oo = zeros(size(slovo_oo, 1), 3);
+f_uu = zeros(size(slovo_uu, 1), 3);
 
 
 %% Matlabov proracun a
@@ -98,6 +104,92 @@ for i = 1:size(f_u, 1)
     % izvlaèenje prva tri formanta
     f_u(i, :) = frqs(1:3);
 end
+
+%% Matlabov proracun a:
+disp('Proraèun za "a:".')
+for i = 1:size(f_aa, 1)
+    % svi korjeni
+    rts = roots(slovo_aa(i, :));
+    
+    % korjeni koji ne leže na realnoj osi
+    rts = rts(imag(rts)>0);
+    angz = atan2(imag(rts),real(rts));
+    
+    % sortiranje formanti
+    [frqs, indices] = sort(angz.*(sampling_frequency/(2*pi)));
+    
+    % izvlaèenje prva tri formanta
+    f_aa(i, :) = frqs(1:3);
+end
+
+%% Matlabov proracun e:
+disp('Proraèun za "e:".')
+for i = 1:size(f_ee, 1)
+    % svi korjeni
+    rts = roots(slovo_ee(i, :));
+    
+    % korjeni koji ne leže na realnoj osi
+    rts = rts(imag(rts)>0);
+    angz = atan2(imag(rts),real(rts));
+    
+    % sortiranje formanti
+    [frqs, indices] = sort(angz.*(sampling_frequency/(2*pi)));
+    
+    % izvlaèenje prva tri formanta
+    f_ee(i, :) = frqs(1:3);
+end
+
+%% Matlabov proracun i:
+disp('Proraèun za "i:".')
+for i = 1:size(f_ii, 1)
+    % svi korjeni
+    rts = roots(slovo_ii(i, :));
+    
+    % korjeni koji ne leže na realnoj osi
+    rts = rts(imag(rts)>0);
+    angz = atan2(imag(rts),real(rts));
+    
+    % sortiranje formanti
+    [frqs, indices] = sort(angz.*(sampling_frequency/(2*pi)));
+    
+    % izvlaèenje prva tri formanta
+    f_ii(i, :) = frqs(1:3);
+end
+
+%% Matlabov proracun o:
+disp('Proraèun za "o:".')
+for i = 1:size(f_oo, 1)
+    % svi korjeni
+    rts = roots(slovo_oo(i, :));
+    
+    % korjeni koji ne leže na realnoj osi
+    rts = rts(imag(rts)>0);
+    angz = atan2(imag(rts),real(rts));
+    
+    % sortiranje formanti
+    [frqs, indices] = sort(angz.*(sampling_frequency/(2*pi)));
+    
+    % izvlaèenje prva tri formanta
+    f_oo(i, :) = frqs(1:3);
+end
+
+%% Matlabov proracun u:
+disp('Proraèun za "u:".')
+for i = 1:size(f_uu, 1)
+    % svi korjeni
+    rts = roots(slovo_uu(i, :));
+    
+    % korjeni koji ne leže na realnoj osi
+    rts = rts(imag(rts)>0);
+    angz = atan2(imag(rts),real(rts));
+    
+    % sortiranje formanti
+    [frqs, indices] = sort(angz.*(sampling_frequency/(2*pi)));
+    
+    % izvlaèenje prva tri formanta
+    f_uu(i, :) = frqs(1:3);
+end
+
 %% plotanje formanti f1 f2
 close all
 figure
@@ -298,4 +390,176 @@ print('slike/novo/U f_2 f_3','-dpng')
 % grid on
 
 %% save
-save('formanti\f_pro.mat', 'f_a', 'f_e', 'f_i', 'f_o', 'f_u')
+save('formanti\f_pro.mat', 'f_a', 'f_e', 'f_i', 'f_o', 'f_u', 'f_aa', 'f_ee', 'f_ii', 'f_oo', 'f_uu')
+
+%%
+%% usporedba x x: plotanje formanti f1 f2
+close all
+figure
+hold on
+scatter(f_a(:, 1), f_a(:, 2), 'r.')
+scatter(f_aa(:, 1), f_aa(:, 2), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 2500]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_e(:, 1), f_e(:, 2), 'r.')
+scatter(f_ee(:, 1), f_ee(:, 2), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 2500]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_i(:, 1), f_i(:, 2), 'r.')
+scatter(f_ii(:, 1), f_ii(:, 2), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 2500]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_o(:, 1), f_o(:, 2), 'r.')
+scatter(f_oo(:, 1), f_oo(:, 2), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 2500]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_u(:, 1), f_u(:, 2), 'r.')
+scatter(f_uu(:, 1), f_uu(:, 2), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 2500]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+%% usporedba x x: plotanje formanti f1 f3
+close all
+figure
+hold on
+scatter(f_a(:, 1), f_a(:, 3), 'r.')
+scatter(f_aa(:, 1), f_aa(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_e(:, 1), f_e(:, 3), 'r.')
+scatter(f_ee(:, 1), f_ee(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_i(:, 1), f_i(:, 3), 'r.')
+scatter(f_ii(:, 1), f_ii(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_o(:, 1), f_o(:, 3), 'r.')
+scatter(f_oo(:, 1), f_oo(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_u(:, 1), f_u(:, 3), 'r.')
+scatter(f_uu(:, 1), f_uu(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+%% usporedba x x: plotanje formanti f2 f3
+close all
+figure
+hold on
+scatter(f_a(:, 2), f_a(:, 3), 'r.')
+scatter(f_aa(:, 2), f_aa(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_e(:, 2), f_e(:, 3), 'r.')
+scatter(f_ee(:, 2), f_ee(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_i(:, 2), f_i(:, 3), 'r.')
+scatter(f_ii(:, 2), f_ii(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_o(:, 2), f_o(:, 3), 'r.')
+scatter(f_oo(:, 2), f_oo(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
+
+figure
+hold on
+scatter(f_u(:, 2), f_u(:, 3), 'r.')
+scatter(f_uu(:, 2), f_uu(:, 3), 'b.')
+xlim([0, 1400]) % Hz
+ylim([0, 3000]) % Hz
+title('A f_1 f_2')
+xlabel('f_1 [Hz]')
+ylabel('f_2 [Hz]')
+grid on
